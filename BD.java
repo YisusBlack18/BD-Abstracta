fca70afc1e44554019d1432852e35493a0496725
YisusBlack18 <jesusangel18jz@gmail.com>
2023-01-30 19:18:40 -0700

    avance

--- BD.java
+++ BD.java
@@ -1,23 +1,40 @@
 import java.sql.Connection;
-// import java.sql.Driver;
+import java.sql.DriverManager;
 
 /**
  * BD
  */
 public abstract class BD {
-    // private Driver controlador;
-    private Connection conexion;
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/";
+    private String user = "root";
+    private String pass = "";
 
-    public Connection conecBD() {
-        this.conexion = null;
+    public BD(String driver, String url, String user, String pass) {
+        this.driver = driver;
+        this.url = url;
+        this.user = user;
+        this.pass = pass;
+    };
+
+
+    public Connection conectBD() {
+        Connection conex = null;
         
-
-        return conexion;
-    }
+        try {
+            Class.forName(driver);
+            conex = DriverManager.getConnection(url, user, pass);
+            
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la base de datos.\n"
+                    + e.getMessage().toString());
+        }
+        return conex;
+    };
 
     public abstract void ejecutaQuery();
 
-
+    public abstract void closeConect();
 
     
 }
---
+++ MySQLBD.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLBD extends BD{
+    
+
+
+
+    public static void main(String[] args) {
+        Connection conex = conectBD();
+        
+    }
+    
+
+    @Override
+    public Connection conectBD() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void ejecutaQuery() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void closeConect() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
