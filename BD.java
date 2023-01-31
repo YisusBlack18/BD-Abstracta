cc6d6c5ea91de4dee1145dee97ae7466287fd5e3
YisusBlack18 <jesusangel18jz@gmail.com>
2023-01-30 17:48:16 -0700

    de mientras

---
+++ BD.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.Driver;
+
+/**
+ * BD
+ */
+public abstract class BD {
+    // private Driver controlador;
+    private Connection conexion;
+
+    public Connection conecBD() {
+        this.conexion = null;
+        
+
+        return conexion;
+    }
+
+    public abstract void ejecutaQuery();
+
+
+
+    
+}
+
