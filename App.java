59a9299b47f91312c8218bf80900aac1cb965a25
YisusBlack18 <jesusangel18jz@gmail.com>
2023-02-02 10:21:16 -0700

    nada

--- App.java
+++ App.java
@@ -5,13 +5,13 @@ import src.PostgreSQL;
 
 public class App {
     public static void main(String[] args) {
-        MySQLBD mySQLBD = new MySQLBD("com.", "localhost", "root", "");
+        MySQLBD mySQLBD = new MySQLBD("com.mysql.jdbc.Driver", "jdbc:mysql://192.168.0.50/", "root", "");
         Connection conex = mySQLBD.conectBD();
         String query = "USE Veterinaria";
         mySQLBD.ejecutaQuery(conex, query);
         mySQLBD.closeConect(conex);
 
-        PostgreSQL postgreSQLBD = new PostgreSQL("com.", "localhost", "root", "");
+        PostgreSQL postgreSQLBD = new PostgreSQL("org.postgresql.Driver", "jdbc:postgresql://192.168.0.50/", "root", "");
         conex = mySQLBD.conectBD();
         query = "USE Veterinaria";
         postgreSQLBD.ejecutaQuery(conex, query);
